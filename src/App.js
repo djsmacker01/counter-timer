4d75b4abb715afe0a9deef568bea1da7d83b9ec4
Success Adedeji Adewale Nurudeen <djsmacker01@gmail.com>
2021-02-23 03:29:09 +0000

    second commit

--- src/App.js
+++ src/App.js
@@ -1,44 +1,48 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 import './App.css';
 
 const App = () => {
-  return (
-    <section className="timer-container">
-      <section className="timer">
-        <div>
-          <span className="mdi mdi-calendar timer-icon">
-            <h2>Countdown timer</h2>
-            <p>Count down your React js Journey</p>
-          </span>
-        </div>
-        <div>
-          <section>
-            <p>87</p>
-            <p><small>Days</small></p>
+
+  const [days, setDays] = useState('00');
+  const [hours, setHours] = useState('00');
+  const [minutes, setMinutes] = useState('00');
+  const [seconds, setSeconds] = useState('00');
+      return (
+        <section className="timer-container">
+          <section className="timer">
+            <div>
+              <span className="mdi mdi-calendar timer-icon"></span>
+              <h2>Countdown timer</h2>
+              <p>Count down your React js Journey</p>
+            </div>
+            <div>
+              <section>
+                <p>87</p>
+                <p><small>Days</small></p>
+              </section>
+              <span>:</span>
+                <section>
+                <p>87</p>
+                <p><small>Hours</small></p>
+              </section>
+              <span>:</span>
+                <section>
+                <p>87</p>
+                <p><small>Minutes</small></p>
+              </section>
+              <span>:</span>
+                <section>
+                <p>87</p>
+                <p><small>Seconds</small></p>
+              </section>
+            </div>
           </section>
-          <span>:</span>
-            <section>
-            <p>87</p>
-            <p><small>Hours</small></p>
-          </section>
-          <span>:</span>
-            <section>
-            <p>87</p>
-            <p><small>Minutes</small></p>
-          </section>
-          <span>:</span>
-            <section>
-            <p>87</p>
-            <p><small>Seconds</small></p>
-          </section>
-        </div>
-      </section>
-           
-      
-    </section>
-    
-  )
-}
+              
+          
+        </section>
+        
+      )
+    }
 
 export default App;
